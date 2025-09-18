4dd0774a22f2c86cf759b7621931020c5e3fce74
Oksana Chertova <chertova_98@internet.ru>
2025-09-18 11:34:54 +0500

    first homework

---
+++ script.js
@@ -0,0 +1,10 @@
+const myCity = "Уфа" // переменная const myCity приравнивание значение Уфа
+console.log(myCity)  // console.log выводит данные в консоль значение myCity
+
+const myTemperature = "+14" // переменная const myTemperature приравниванит значение +14
+console.log(myTemperature) // console.log выводит данные в консоль значение myTemperature
+
+const x = 5 // переменные const x, y, z приравниванию значения 5, 3, 2
+const y = 3 
+const z = 2
+console.log( (x + y) * z) // console.log выводит выражение (x + y) * z в консоль с получаемым значением с использованием операторов

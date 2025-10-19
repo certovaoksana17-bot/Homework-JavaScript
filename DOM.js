313c8498cada348cefa6e0ba433d8b81d60db2f8
Oksana Chertova <chertova_98@internet.ru>
2025-10-19 16:22:49 +0500

    Взаимодействие JS с DOM

---
+++ DOM.js
@@ -0,0 +1,45 @@
+const myCity = document.getElementById('city')
+console.log(myCity.innerHTML)
+
+document.getElementById('city').textContent = 'Новосибирск'
+console.log(myCity.innerHTML)
+
+const redItems = document.querySelector('.items');
+
+redItems.style.color = 'red';
+redItems.style.backgroundColor = 'blue';
+console.log(redItems.innerHTML)
+
+const newParagraph = document.createElement('p');
+newParagraph.textContent = 'Новый созданный параграф';
+document.body.appendChild(newParagraph);
+console.log(newParagraph)
+
+
+function removeElementById(elementId) {
+    const element = document.getElementById(elementId);
+
+    if (element) {
+        element.remove();
+    } else {
+        console.warn(`Элемент с ID "${id}" не найден.`);
+    }
+}
+removeElementById("city"); 
+
+const link = document.querySelector('a');
+link.setAttribute('href', 'https://www.yandex.ru');
+console.log(link.setAttribute('href', 'https://www.yandex.ru'));
+
+
+const newElement = document.createElement('div');
+newElement.textContent = 'Новый созданный элемент';
+document.body.appendChild(newElement)
+console.log(newElement)
+
+newElement.classList.add('new-class');
+console.log(newElement.innerHTML)
+
+const colors = document.getElementById('main');
+colors.classList.toggle('pink')
+console.log(colors)
